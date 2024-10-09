46a12c5ac6ef9faae47e6ba2b54673e22dfc1a42
Jakub Pavlik <severus@post.cz>
2024-10-09 16:28:55 +0200

    split huge function

--- lilygabc.scm
+++ lilygabc.scm
@@ -25,6 +25,45 @@
 
 (define flatten (cut apply append <>))
 
+(define (make-notes clef syllables)
+  (make-music
+   'ContextSpeccedMusic
+   'context-id
+   "uniqueContext0"
+   'context-type
+   'Voice
+   'element
+   (make-music
+    'SequentialMusic
+    'elements
+    (flatten
+     (map
+      (lambda (syllable)
+        (let* ((notes (filter (lambda (x) (eq? 'note (car x))) syllable))
+               (is-melisma (< 1 (length notes)))
+               (is-melisma-beginning (lambda (i) (= 1 i)))
+               (is-melisma-end (lambda (i) (= (length notes) i)))
+               (note-i 0))
+          (filter-map
+           (lambda (item)
+             (case (first item)
+               ((note)
+                (set! note-i (+ 1 note-i))
+                (make-ly-note
+                 (gabc-note-to-pitch clef (second item))
+                 (if is-melisma
+                     (cond ((is-melisma-beginning note-i) -1)
+                           ((is-melisma-end note-i) 1)
+                           (else #f))
+                     #f)))
+               ((divisio)
+                (make-music 'BarEvent 'bar-type
+                            (or (assoc-ref divisiones-mapping (second item))
+                                default-bar)))
+               (else #f)))
+           syllable)))
+      syllables)))))
+
 (define (make-ly-note pitch slur-direction)
   (apply
    make-music
@@ -37,6 +76,42 @@
     (list
      'duration (ly:make-duration 2)
      'pitch pitch))))
+
+(define (make-lyrics syllables)
+  (make-music
+   'ContextSpeccedMusic
+   'element
+   (make-music
+    'LyricCombineMusic
+    'associated-context-type
+    'Voice
+    'associated-context
+    "uniqueContext0"
+    'element
+    (make-music
+     'SequentialMusic
+     'elements
+     (flatten
+      (filter-map
+       (lambda (syllable)
+         (let ((lyr (first syllable)))
+           (if (eq? 'lyrics (first lyr))
+               (list
+                (make-music
+                 'LyricEvent
+                 'text
+                 (second lyr)
+                 'duration
+                 (ly:make-duration 2))
+                (make-music 'CompletizeExtenderEvent))
+               #f)))
+       syllables))))
+   'property-operations
+   '()
+   'context-type
+   'Lyrics
+   'create-new
+   #t))
 
 ; accepts string containing gabc notation,
 ; returns LilyPond music
@@ -53,77 +128,8 @@
        'SimultaneousMusic
        'elements
        (list
-        (make-music
-         'ContextSpeccedMusic
-         'context-id
-         "uniqueContext0"
-         'context-type
-         'Voice
-         'element
-         (make-music
-          'SequentialMusic
-          'elements
-          (flatten
-           (map
-            (lambda (syllable)
-              (let* ((notes (filter (lambda (x) (eq? 'note (car x))) syllable))
-                     (is-melisma (< 1 (length notes)))
-                     (is-melisma-beginning (lambda (i) (= 1 i)))
-                     (is-melisma-end (lambda (i) (= (length notes) i)))
-                     (note-i 0))
-                (filter-map
-                 (lambda (item)
-                   (case (first item)
-                     ((note)
-                      (set! note-i (+ 1 note-i))
-                      (make-ly-note
-                       (gabc-note-to-pitch clef (second item))
-                       (if is-melisma
-                           (cond ((is-melisma-beginning note-i) -1)
-                                 ((is-melisma-end note-i) 1)
-                                 (else #f))
-                           #f)))
-                     ((divisio)
-                      (make-music 'BarEvent 'bar-type
-                                  (or (assoc-ref divisiones-mapping (second item))
-                                      default-bar)))
-                     (else #f)))
-                 syllable)))
-            syllables))))
-        (make-music ; lyrics
-         'ContextSpeccedMusic
-         'element
-         (make-music
-          'LyricCombineMusic
-          'associated-context-type
-          'Voice
-          'associated-context
-          "uniqueContext0"
-          'element
-          (make-music
-           'SequentialMusic
-           'elements
-           (flatten
-            (filter-map
-             (lambda (syllable)
-               (let ((lyr (first syllable)))
-                 (if (eq? 'lyrics (first lyr))
-                     (list
-                      (make-music
-                       'LyricEvent
-                       'text
-                       (second lyr)
-                       'duration
-                       (ly:make-duration 2))
-                      (make-music 'CompletizeExtenderEvent))
-                     #f)))
-             syllables))))
-         'property-operations
-         '()
-         'context-type
-         'Lyrics
-         'create-new
-         #t))))))
+        (make-notes clef syllables)
+        (make-lyrics syllables))))))
 
 (define music-from-gabc-file
   (define-scheme-function
