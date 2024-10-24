ba25e8882542a405a5784baade2ef86d7492b001
Jakub Pavlik <severus@post.cz>
2024-10-24 18:09:20 +0200

    fix regression test

--- tests/regression/test.sh
+++ tests/regression/test.sh
@@ -6,6 +6,6 @@ echo "= Modern notation"
 echo "== LilyPond"
 ../../bin/gabc2ly $INPUT 2> /dev/null | (diff --color standard.ly - && echo "Output matches")
 echo "== Scheme"
-../../bin/gabc2scm $INPUT 2> /dev/null | diff --color standard.scm -
+../../bin/gabc2ly --scheme $INPUT 2> /dev/null | diff --color standard.scm -
 
 # TODO square notation
