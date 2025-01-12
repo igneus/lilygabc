5bab8ba226a2a0356fcd41c93d66e73f5220f895
Jakub Pavlik <severus@post.cz>
2025-01-12 13:21:58 +0100

    refactor to the standard library

--- lilygabc/lyrics.scm
+++ lilygabc/lyrics.scm
@@ -25,14 +25,13 @@
     ("+" . "†")))
 
 (define-public (expand-special-chars str)
-  (let ((m (regexp-exec sp-tag-re str)))
-    (if m
-        (string-append
-         (substring str 0 (match:start m))
-         (or (assoc-ref special-chars (match:substring m 1))
-             (match:substring m 0))
-         (expand-special-chars (substring str (match:end m))))
-        str)))
+  (regexp-substitute/global
+   #f sp-tag-re str
+   'pre
+   (lambda (m)
+     (or (assoc-ref special-chars (match:substring m 1))
+         (match:substring m 0)))
+   'post))
 
 (define-public (remove-tags str)
   (regexp-substitute/global #f "<[^>]*>" str 'pre "" 'post))
